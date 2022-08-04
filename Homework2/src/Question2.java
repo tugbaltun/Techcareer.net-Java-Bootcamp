2f219176a3ddb4c2c35c501eba91c8956fb233d0
Tugba Altun <36510900+tugbaltun@users.noreply.github.com>
2022-08-04 22:41:27 +0300

    Homework2

--- Homework2/src/Question2.java
+++ Homework2/src/Question2.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
 public class Question2 {
+
+    /*str = Car races were started in France in 1894. Today, they are one of  the most popular audience sports.
+    Competition is something that cars attend and results are announced and issued. The competition has a
+    wholeness and it is original in its own outcomes. It can be combined with a couple of level and a final.
+    Those were the information that Hilmi got from Wikipedia. He is really into the car races. He watches them
+    on T.V whenever he can. He is planning to go and watch a race next weekend and he is just really excited.*/
     public static void main(String[] arg) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter the text:");
         String str = input.nextLine();
+        System.out.println("Enter search word:");
+        String searchWord = input.nextLine();
 
-        String split[] = str.split(" ", 0);
-        System.out.println("Number of words in the text:"+split.length);
+        String split[] = str.split(searchWord, -1);
+        System.out.println("Number of '"+searchWord+"' in the text:"+(split.length-1));
+
         input.close();
     }
 }
--- Homework2/src/Question3.java
+++ Homework2/src/Question3.java
@@ -1,24 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 public class Question3 {
 
-    private static int countingTheElements(List<String> myList){
-        int counter = 0;
-        String split[];
-        for(String list:myList){
-            split = list.split(" ", 0);
-            counter+=split.length;
+    private static String str = "Car races were started in France in 1894. Today, they are one of  the most popular " +
+            "audience sports.Competition is something that cars attend and results are announced and issued. The competition has a " +
+            "wholeness and it is original in its own outcomes. It can be combined with a couple of level and a final."+
+            "Those were the information that Hilmi got from Wikipedia. He is really into the car races. He watches them"+
+            "on T.V whenever he can. He is planning to go and watch a race next weekend and he is just really excited";
+    private static void countingTheElements(String str){
+        ArrayList<String> myList = new ArrayList<>();
+        String splitArray[] = str.toLowerCase().split(" ");
+
+        for(int i=0;i<splitArray.length;i++) {
+            int count=0;
+            for(int j=0;j<splitArray.length;j++) {
+
+                String temp1 = splitArray[i];
+                String temp2 = splitArray[j];
+
+                if(j<i && temp1.equals(temp2)) {
+                    break;
+                }
+
+                if(temp1.equals(temp2)) {
+                    count++;
+                }
+
+                if(j==splitArray.length-1) {
+                    System.out.println(splitArray[i]+" "+count);
+                }
+            }
         }
-        return counter;
+
     }
 
     public static void main(String[] arg) {
 
-        List<String> myList = new ArrayList<String>();
-        myList.add("Happy New Year");
-        myList.add("Coding Interview");
-
-        System.out.println("Number of element in the list:"+countingTheElements(myList));
+        countingTheElements(str);
     }
 }

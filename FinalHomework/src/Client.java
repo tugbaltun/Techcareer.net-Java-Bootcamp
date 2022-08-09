072361666d23656de588ea26aff0a40cb4865d25
Tugba Altun <36510900+tugbaltun@users.noreply.github.com>
2022-08-09 18:40:08 +0300

    FinalHomework first commit

--- FinalHomework/src/Client.java
+++ FinalHomework/src/Client.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
 
--- FinalHomework/src/WriteThread.java
+++ FinalHomework/src/WriteThread.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
